56b4c7d47b09dafecaae3377b2369329a724d3db
Enrique Saracho Felix <esarachof97@gmail.com>
2022-01-07 13:40:46 -0700

    Update app.js index.html styles.css
    Updated all files

---
+++ app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let alienInvadersTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderId
+
+    //define the alien invaders
+    const alienInvaders = [
+        0,1,2,3,4,5,6,7,8,9,
+        15,16,17,18,19,20,21,22,23,24,
+        30,31,32,33,34,35,36,37,38,39
+    ]
+    //draw the alien invaders
+    alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
+    //draw the shooter
+    squares[currentShooterIndex].classList.add('shooter')
+
+    //move the shooter along a line
+    function moveShooter(e) {
+        squares[currentShooterIndex].classList.remove('shooter') //removes shooter class from div (position)
+        switch(e.keyCode) {
+            case 37:
+                if (currentShooterIndex % width !== 0) currentShooterIndex -= 1 //moves shooter to the left if possible
+                break
+            case 39:
+                if (currentShooterIndex % width < width - 1) currentShooterIndex += 1 //moves shooter to the right if possible
+                break
+        }
+        squares[currentShooterIndex].classList.add('shooter') //adds shooter class to new div (position)
+    }
+    document.addEventListener('keydown', moveShooter)
+})
